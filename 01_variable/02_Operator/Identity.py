8faecb1a59fc983b15bc69e7aac723f693519176
Prince Yadav <py7496813@gmail.com>
2025-06-06 11:45:08 +0530

    Create Identity.py

---
+++ 01_variable/02_Operator/Identity.py
@@ -0,0 +1,7 @@
+a=int(input("Enter the first number:"))             ## 5
+b=int(input("Enter the second number:"))            ## 6
+c=int(input("Enter the third number:"))             ## 2
+print(a is not b)        ## True
+print(a is b)          ## False
+print(a is c)         ## False
+print(a is not c)        ## True

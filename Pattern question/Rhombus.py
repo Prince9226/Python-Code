63f33d965a551386881f8466fd717a759ab1d30f
Prince Yadav <py7496813@gmail.com>
2025-06-08 11:18:35 +0530

    Create Rhombus.py

---
+++ Pattern question/Rhombus.py
@@ -0,0 +1,15 @@
+num=3
+for i in range(num):
+    for j in range(num-i-1):
+        print(" ",end=" ")
+    for j in range(2*1+1):
+        print("*", end=" ")
+    print()
+
+      ## Output
+
+'''
+    * * * 
+  * * * 
+* * * 
+'''

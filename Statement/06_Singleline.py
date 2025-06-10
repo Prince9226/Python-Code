33eed69980a238d98b328b2781ab5c954eda92d5
Prince Yadav <py7496813@gmail.com>
2025-06-10 15:39:46 +0530

    Create 06_Singleline.py

---
+++ Statement/06_Singleline.py
@@ -0,0 +1,9 @@
+age=int(input("Enter your age:"))
+print("Congratulations! you can drive the car.") if(age>=18) else print("you can not drive the car!")
+
+
+    ## Output
+'''
+Enter your age:18
+Congratulations! you can drive the car.
+'''

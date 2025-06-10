f1262cd6089f38bb4df869f776440ad1729848d3
Prince Yadav <py7496813@gmail.com>
2025-06-10 16:43:40 +0530

    Create 05_Reverse_table.py

---
+++ Loop/05_Reverse_table.py
@@ -0,0 +1,11 @@
+num=int(input("Enter the number for reverse table:"))
+i=10
+while(i > 0):
+  print(num*i,end=" ")
+  i -= 1
+
+## Output
+'''
+Enter the number for reverse table:4
+40 36 32 28 24 20 16 12 8 4 
+'''

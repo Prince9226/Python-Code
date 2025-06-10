680310bc75a81c369e9b5b370fe84fab304e7337
Prince Yadav <py7496813@gmail.com>
2025-06-10 15:55:54 +0530

    Create 01_while.py

---
+++ Loop/01_while.py
@@ -0,0 +1,16 @@
+num=int(input("Enter the number:"))
+i=0
+while(i < num):
+  print("Hello world!")
+  i  += 1
+
+
+## Output
+'''
+Enter the number:5
+Hello world!
+Hello world!
+Hello world!
+Hello world!
+Hello world!
+'''

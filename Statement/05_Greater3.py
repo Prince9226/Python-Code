7c548ef55400e0cc2473232300ec2dc583fc9435
Prince Yadav <py7496813@gmail.com>
2025-06-10 15:30:57 +0530

    Create 05_Greater3.py

---
+++ Statement/05_Greater3.py
@@ -0,0 +1,17 @@
+num1=int(input("Enter the first number:"))
+num2=int(input("Enter the second number:"))
+num3=int(input("Enter the third number:"))
+if(num1 < num2 > num3):
+  print(num1,"is greater")
+elif(num2 < num1 > num3):
+  print(num1, "is greater")
+else:
+  print(num3, "is greater")
+
+## Output
+'''
+Enter the first number:25
+Enter the second number:4
+Enter the third number:6
+25 is greater
+'''

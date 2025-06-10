3c5f2b5c263fd045f8692204d2a3486650fe12e6
Prince Yadav <py7496813@gmail.com>
2025-06-10 15:19:02 +0530

    Create 03_Greater.py

---
+++ Statement/03_Greater.py
@@ -0,0 +1,15 @@
+num1=int(input("Enter the first numbr:"))
+num2=int(input("Enter the second number:"))
+if(num2>num1):
+  print(num2 , "is greater")
+else:
+  print(num1, "is greater")
+
+
+## Output
+'''
+Enter the first numbr:25
+Enter the second number:30
+30 is greater
+'''
+               

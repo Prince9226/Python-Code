fa392679b5894e26a90df4abe547e22714153901
Prince Yadav <py7496813@gmail.com>
2025-08-22 19:30:24 +0530

    Create 03_second_method.py

---
+++ 07_String/03_second_method.py
@@ -0,0 +1,7 @@
+n=input("Enter the name:")
+print(n.upper())   # name capital letter
+print(n.title())   # name of charcter first letter capital
+print(n.lower())    # name small letter
+print(n.find('a',2))
+print(n.replace('r','m'))     # all r replace and present m
+print(n.replace('r','m',2))   # only first 2 r replace

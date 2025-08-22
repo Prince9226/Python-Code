d2d8f19de645bb108edef17ded12fb58628808ad
Prince Yadav <py7496813@gmail.com>
2025-08-22 19:28:17 +0530

    Create 01_first_method.py

---
+++ 07_String/01_first_method.py
@@ -0,0 +1,6 @@
+s1=("helloprince")
+print(s1.index("o"))     #find the number of index.
+print(s1.endswith('e'))  # In Present string ends with 'e' then True otherwise False.
+print(s1.isdigit())      # In Present string  digit then True otherwise False.
+print(s1.islower())      # In Present all string are small letter then True otherwise false.
+print(s1.count('l'))      # In Present string count the 'l' and result always number. 

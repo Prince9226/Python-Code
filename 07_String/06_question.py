e41352aeb2ca319c76b8ae8031a23c850f3b797b
Prince Yadav <py7496813@gmail.com>
2025-08-22 19:33:32 +0530

    Create 06_question.py

---
+++ 07_String/06_question.py
@@ -0,0 +1,7 @@
+ Write a program to take different words separated by comma and print reverse order of their occurrence.
+
+s1=input("Enter your words:")
+x=s1.split(",")
+##print(len(x))
+y=x.reverse()  # reverse charcter or name
+print("reverse order of their occurrence",x)

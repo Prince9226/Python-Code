dca79bb586ba1bc565fca8919c37c78f42cb14ae
Prince Yadav <py7496813@gmail.com>
2025-08-22 19:29:04 +0530

    Create 02_check_vowel.py

---
+++ 07_String/02_check_vowel.py
@@ -0,0 +1,33 @@
+n=input("enter the str")
+if("A" in n):
+    a=1
+    print("A")
+if("a" in n):
+    a=1
+    print("a")
+if("e" in n):
+    a=1
+    print("e")
+if("i" in n):
+    a=1
+    print("i")
+if("o" in n):
+    a=1
+    print("o")
+if("u" in n):
+    a=1
+    print("u")
+if("E" in n):
+    a=1
+    print("E")
+if("U" in n):
+    a=1
+    print("U")
+if("O" in n):
+    a=1
+    print("O")
+if("I" in n):
+    a=1
+    print("I")
+else:
+    print("no vowel")

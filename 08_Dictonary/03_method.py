e71090b1fe4827e71ff348d0ae47c052db1c1055
Prince Yadav <py7496813@gmail.com>
2025-08-22 19:42:34 +0530

    Create 03_method.py

---
+++ 08_Dictonary/03_method.py
@@ -0,0 +1,10 @@
+d1={
+    'a':12,
+    'class':'bca',
+    'name':'prince'
+}
+print(d1.keys())   ## print only keys
+print(d1.values())  ## print values
+print(d1.items())    ## print all items
+print(d1.pop('class'))  ## print values of class
+print(d1.get('a',''))    ## print values of a

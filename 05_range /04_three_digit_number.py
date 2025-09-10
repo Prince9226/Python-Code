74913827b7b8b57198502841673ac663810bc6b6
Prince Yadav <py7496813@gmail.com>
2025-09-10 22:18:33 +0530

    range

--- 05_range /04_three_digit_number.py
+++
@@ -1,8 +0,0 @@
-n=int(input("Enter the no."))   ## n even natural number
-for i in range(2,n,2):
-      print(i)'''
-n= int(input("Enter the number"))
-if(n>=100 and n<999):        ## r1=(100,999)    if(n in r1):
-      print("yes three digit number")
-else:
-      print("no three digit number")

f055f765e1f207fe7cec0a0a4a3edbd53ff01398
Prince Yadav <py7496813@gmail.com>
2025-09-14 20:35:08 +0530

    Reverse using split

--- 07_String/06_question.py
+++ 07_String/06_question.py
@@ -1,4 +1,4 @@
- Write a program to take different words separated by comma and print reverse order of their occurrence.
+ #Write a program to take different words separated by comma and print reverse order of their occurrence.
 
 s1=input("Enter your words:")
 x=s1.split(",")
